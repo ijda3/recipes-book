682f2446468a17c62231d32c90146427c3fb5231
Itamar Jr <itamar.jda@gmail.com>
2020-06-09 15:25:31 -0300

    Add prev and next recipe with ncurses

--- main.c
+++ main.c
@@ -1,3 +1,34 @@
-#include "recipe.h"
+#include "src/lib/recipes/recipe.h"
+#include <ncurses.h>
+#include <stdlib.h>
 
-int main() { return 0; }
+#define ESCAPE 27
+
+int main() {
+	int key;
+	Recipes* recipes = load_recipes();
+
+	initscr();
+	keypad(stdscr, TRUE);
+	noecho();
+
+	do {
+		key = getch();
+
+		switch (key) {
+			case KEY_LEFT:
+				prev_recipe(recipes);
+				break;
+			case KEY_RIGHT:
+				next_recipe(recipes);
+				break;
+		}
+
+		refresh();
+	} while (key != ESCAPE);
+
+	free(recipes);
+	endwin();
+
+	return 0;
+}
