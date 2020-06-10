dd152f6087eadb65558044b99b4f29fa961c9b6f
Itamar Jr <itamar.jda@gmail.com>
2020-06-09 22:16:01 -0300

    Add navigation

--- main.c
+++ main.c
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 
 #define ESCAPE 27
+#define KEY_USE 42
+#define KEY_EDIT 97
+#define KEY_REMOVE 33
+#define KEY_ADD 43
+#define KEY_SAIR 83
+
+void menu() {
+	mvprintw(0, 0, "LIVRO DE RECEITAS");
+	mvprintw(2, 0, "Opções:");
+	mvprintw(3, 0, "< > - Navegar");
+	mvprintw(4, 0, "* - Marcar como usada");
+	mvprintw(5, 0, "a - Alterar");
+	mvprintw(6, 0, "! - Apagar receita");
+	mvprintw(7, 0, "+ - Inserir receita");
+	mvprintw(8, 0, "S - Sair do livro de receitas");
+}
 
 int main() {
 	int key;
@@ -12,8 +28,12 @@ int main() {
 	keypad(stdscr, TRUE);
 	noecho();
 
+	menu();
+
 	do {
+		move(11, 0);
 		key = getch();
+		clrtoeol();
 
 		switch (key) {
 			case KEY_LEFT:
@@ -22,10 +42,22 @@ int main() {
 			case KEY_RIGHT:
 				next_recipe(recipes);
 				break;
+			case KEY_ADD:
+				add_recipe(recipes);
+				break;
+			case KEY_REMOVE:
+				remove_recipe(recipes);
+				break;
+			case KEY_EDIT:
+				edit_recipe(recipes);
+				break;
+			case KEY_USE:
+				use_recipe(recipes);
+				break;
 		}
+	} while (key != KEY_SAIR);
 
-		refresh();
-	} while (key != ESCAPE);
+	save_recipes(recipes);
 
 	free(recipes);
 	endwin();
